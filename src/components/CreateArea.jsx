baa5819b036d974237ee47043dd1e638082a8fe1
Anini Mishra <anini.mishra0611@gmail.com>
2021-07-08 00:58:13 +0530

    styling-final commit.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded,setExpand] = useState(false);
+
   const [fullNote, addNote] = useState({
     title: "",
     content: ""
@@ -20,24 +25,30 @@ function CreateArea(props) {
     });
     event.preventDefault();
   }
+  function expand(){
+    setExpand(true);
+  }
 
   return (
     <div>
-      <form>
-        <input
+      <form className="create-note">
+        {isExpanded && (<input
           onChange={handleChange}
           name="title"
           placeholder="Title"
           value={fullNote.title}
-        />
+        />)}
         <textarea
           onChange={handleChange}
+          onClick={expand}
           name="content"
           placeholder="Take a note..."
           value={fullNote.content}
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={handleSubmit}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={handleSubmit}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
